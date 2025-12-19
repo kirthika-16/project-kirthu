dce5c5ad8ad0f53140e99f67838a77ac0804ffbc
kirthika-16 <kirthika2005k@gmail.com>
2025-12-19 12:52:56 +0530

    version 1

---
+++ Calculation.py
@@ -0,0 +1,9 @@
+print("welcome to Calculator!")
+Name=str(input("Enter Your Name:"))
+print("Welcome ",Name,"!")
+A=int(input("Enter The First Number :"))
+B=int(input("Enter The Second Number :"))
+Addition=A+B
+Subraction=A-B
+print("Addition Of Two Numbers:",Addition)
+print("Subraction Of Two Numbers:",Subraction) 
